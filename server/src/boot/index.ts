273063fe07647d577c8248339fcf50e32ad80174
Samal <samalgorai.work@gmail.com>
2025-11-16 09:42:20 +0530

    Creates unified build, run as a single instance.

--- server/src/boot/index.ts
+++ server/src/boot/index.ts
@@ -1,4 +1,6 @@
-import { Application } from 'express';
+import express, { Application } from 'express';
+import fs from 'fs';
+import path from 'path';
 import cors, { CorsOptions } from 'cors';
 
 import { loggerService } from '../utilities';
@@ -23,8 +25,36 @@ const plugIpLogging = (app: Application): void => {
   });
 };
 
+const plugBuild = (app: Application): void => {
+  try {
+    const staticBuildPath = process.env.UI_BUILD_PATH || '../../ui';
+    // eslint-disable-next-line global-require, import/no-dynamic-require
+    app.use(express.static(path.join(__dirname, `${staticBuildPath}`)));
+    app.use('/_next', express.static(path.join(__dirname, staticBuildPath)));
+
+    app.get('*', (req, res, next) => {
+      const { path: reqPath, query } = req;
+
+      const staticPath = path.resolve(__dirname, staticBuildPath) + `${reqPath}.html`.replace('/.', '.');
+      if (query.api_token) return next();
+      fs.readFile(staticPath, 'utf8', (err, data) => {
+        if (err) {
+          return next();
+        }
+        if (data) {
+          return res.send(data);
+        }
+        return next();
+      });
+    });
+  } catch (e) {
+    loggerService.error('Init::UI path failed', e);
+  }
+};
+
 export default (app: Application, IP_LOGGING: boolean): void => {
   plugCors(app);
+  plugBuild(app);
   if (IP_LOGGING) {
     plugIpLogging(app);
   }
